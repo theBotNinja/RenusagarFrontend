369c9417e5c157c867d92e34f076d24e8b1f5226
devansh joshi <devansh0814dj@gmail.com>
2025-06-21 00:08:19 +0530

    quick change to home file for white background

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./UiElements/Navbar";
 import Footer from "./UiElements/Footer";
 import ImageStrip from "./ImageStrip";
@@ -7,17 +7,21 @@ import FacilitiesSection from "./FacilitiesSection";
 import HeroSection from "./HeroSection";
 import SchoolSection from "./SchoolSection";
 import Tourism from "./Tourism";
+import { Theme } from "../Context/Theme";
 export default function Home() {
+  const [isDark, changetheme] = useContext(Theme);
   return (
     <>
-      <Navbar></Navbar>
-      <HeroSection></HeroSection>
-      <ImageStrip></ImageStrip>
-      <IndustrySection></IndustrySection>
-      <FacilitiesSection></FacilitiesSection>
-      <SchoolSection></SchoolSection>
-      <Tourism></Tourism>
-      <Footer></Footer>
+      <div className={isDark ? "bg-neutral-900" : "bg-white"}>
+        <Navbar></Navbar>
+        <HeroSection></HeroSection>
+        <ImageStrip></ImageStrip>
+        <IndustrySection></IndustrySection>
+        <FacilitiesSection></FacilitiesSection>
+        <SchoolSection></SchoolSection>
+        <Tourism></Tourism>
+        <Footer></Footer>
+      </div>
     </>
   );
 }
